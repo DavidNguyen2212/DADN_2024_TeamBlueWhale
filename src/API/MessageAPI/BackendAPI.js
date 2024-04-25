d6e29326999e314db36fbb98cf2a5987b7adf4ae
DavidNguyen2212 <annhonguoiyeu@gmail.com>
2024-04-25 10:15:33 +0700

    Fix new link to render.com

--- src/API/MessageAPI/BackendAPI.js
+++ src/API/MessageAPI/BackendAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BackendAPI = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+    baseURL: "https://dadn-2024-backend.onrender.com",
     headers: {
     "Content-Type": "application/json",
   },
